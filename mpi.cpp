36c4466d971f82e55ca59efb075cce07b69d0d4d
ManassehV7809 <104124845+ManassehV7809@users.noreply.github.com>
2023-05-25 04:32:16 +0200

    Update mpi.cpp

--- mpi.cpp
+++ mpi.cpp
@@ -1,104 +1,98 @@
 #include <iostream>
-#include <limits>
 #include <vector>
-#include <omp.h>
+#include <limits>
 #include <mpi.h>
 
 using namespace std;
-#define INF numeric_limits<int>::max()
 
-void parallelDijkstra(const vector<int> &graph, int source, vector<int> &distance) {
-    int numVertices = graph.size();
-    int numProcesses, rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+#define Infinity numeric_limits<int>::max()
 
-    int numLocalVertices = numVertices / numProcesses;
-    int startVertex = rank * numLocalVertices;
-    int endVertex = startVertex + numLocalVertices - 1;
-    if (rank == numProcesses - 1) {
-        endVertex = numVertices - 1;
-        numLocalVertices = endVertex - startVertex + 1;
-    }
+void Dijkstra(const vector<vector<int>> &graph, int source, vector<int> &distance, int numProcesses, int rank) {
+    int numVerts = graph.size();
+    vector<bool> visited(numVerts, false);
 
-    vector<bool> visited(numVertices, false);
-    vector<int> localDistance(numLocalVertices, INF);
+    // Set the distance of the source vertex to 0
+    distance[source] = 0;
 
-    // Initialize local distance array
-    for (int i = startVertex; i <= endVertex; ++i) {
-        if (i == source)
-            localDistance[i - startVertex] = 0;
-        else
-            localDistance[i - startVertex] = INF;
-    }
+    // Main loop
+    for (int i = 0; i < numVerts - 1; ++i) {
+        int u = -1;
+        int minDist = Infinity;
 
-    for (int i = 0; i < numVertices; ++i) {
-        int u, minDist = INF;
-        
         // Find the vertex with the minimum distance among unvisited vertices
-        for (int j = startVertex; j <= endVertex; ++j) {
-            if (!visited[j] && localDistance[j - startVertex] < minDist) {
-                minDist = localDistance[j - startVertex];
-                u = j;
+        for (int v = 0; v < numVerts; ++v) {
+            if (!visited[v] && distance[v] < minDist) {
+                minDist = distance[v];
+                u = v;
             }
         }
 
-        // Broadcast the minimum distance and vertex from the process that owns it
-        int minDistProcess;
-        MPI_Allreduce(&minDist, &minDistProcess, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
-        MPI_Bcast(&u, 1, MPI_INT, minDistProcess / numLocalVertices, MPI_COMM_WORLD);
+        // Broadcast the selected vertex u to all processes
+        MPI_Bcast(&u, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-        if (u >= startVertex && u <= endVertex) {
-            visited[u] = true;
+        if (u == -1) {
+            break;
+        }
 
-            // Relax edges connected to the selected vertex
-            #pragma omp parallel for
-            for (int v = 0; v < numVertices; ++v) {
-                if (!visited[v] && graph[u][v] != INF) {
-                    int distThroughU = localDistance[u - startVertex] + graph[u][v];
-                    if (distThroughU < localDistance[v - startVertex])
-                        localDistance[v - startVertex] = distThroughU;
-                }
+        visited[u] = true;
+
+        // Relax edges connected to the selected vertex
+        for (int v = 0; v < numVerts; ++v) {
+            if (!visited[v] && graph[u][v] != Infinity) {
+                int distThroughU = distance[u] + graph[u][v];
+                distance[v] = min(distance[v], distThroughU);
             }
         }
+
+        // Reduce the distance vector across all processes
+        MPI_Allreduce(MPI_IN_PLACE, distance.data(), numVerts, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
     }
-
-    // Gather local distances from all processes
-    vector<int> allDistances(numVertices);
-    MPI_Allgather(&localDistance[0], numLocalVertices, MPI_INT, &allDistances[0], numLocalVertices, MPI_INT, MPI_COMM_WORLD);
-
-    // Copy local distances to the output distance array
-    for (int i = 0; i < numVertices; ++i)
-        distance[i] = allDistances[i];
 }
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
-    int rank;
+    int rank, numProcesses;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 
     // Example graph
     vector<vector<int>> graph = {
-        {0, 2, INF, 1},
-        {2, 0, 1, INF},
-        {INF, 1, 0, 1},
-        {1, INF, 1, 0}
+        {0, 2, Infinity, 1},
+        {2, 0, 1, Infinity},
+        {Infinity, 1, 0, 1},
+        {1, Infinity, 1, 0}
     };
 
     int numVertices = graph.size();
-    vector<int> distance(numVertices);
-
     int source = 0;  // Source vertex
 
-    parallelDijkstra(graph, source, distance);
+    // Divide the vertices among processes
+    int verticesPerProcess = numVertices / numProcesses;
+    int startVertex = rank * verticesPerProcess;
+    int endVertex = startVertex + verticesPerProcess;
 
+    vector<int> distance(numVertices, Infinity);  // Resize the distance vector
+
+    // Compute shortest paths using Dijkstra algorithm
+    Dijkstra(graph, source, distance, numProcesses, rank);
+
+    // Gather and print the shortest distances
     if (rank == 0) {
+        vector<int> allDistances(numVertices);
+        MPI_Gather(distance.data() + startVertex, verticesPerProcess, MPI_INT,
+                   allDistances.data(), verticesPerProcess, MPI_INT, 0, MPI_COMM_WORLD);
+
         cout << "Shortest distances from source vertex " << source << ":" << endl;
-        for (int i = 0; i < numVertices; ++i)
-            cout << "Vertex " << i << ": " << distance[i] << endl;
+        for (int i = 0; i < numVertices; i++) {
+            cout << "Vertex " << i << ": " << allDistances[i] << endl;
+        }
+    } else {
+        MPI_Gather(distance.data() + startVertex, verticesPerProcess, MPI_INT,
+                   nullptr, 0, MPI_INT, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
+
     return 0;
 }
